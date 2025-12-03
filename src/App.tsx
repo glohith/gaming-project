43ce43dec53c7c20ca7251c48dd7a7c0163b8d8c
glohith <varshini.1si16is061@gmail.com>
2025-12-03 05:48:00 +0530

    intial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Button, HStack } from "@chakra-ui/react"
+import { RiArrowRightLine, RiMailLine } from "react-icons/ri"
+
+function App() {
+  
+
+  return (
+
+    <HStack>
+      <Button colorPalette="teal" variant="solid">
+        <RiMailLine /> Email
+      </Button>
+      <Button colorPalette="teal" variant="outline">
+        Call us <RiArrowRightLine />
+      </Button>
+    </HStack>
+    
+  )
+}
+
+export default App
