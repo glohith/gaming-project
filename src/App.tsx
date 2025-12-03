347a07020ba314e15fde4cffcc089e629c1c76fc
glohith <glohith5214@gamil.com>
2025-12-04 04:38:33 +0530

    build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-import GamerForm from "./components/GameForm";
+import { Grid, GridItem } from "@chakra-ui/react";
 
-function app (){
-
-  return(
+function app() {
+  return (
     <>
-    <GamerForm />
+      <Grid
+        templateAreas={{
+          base: `"nav" " main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area={`nav`} bg={`coral`}>
+          nav
+        </GridItem>
+
+        <GridItem
+          area={`aside`}
+          bg={`gold`}
+          display={{ base: "none", lg: "block" }}
+        >
+          Aside
+        </GridItem>
+
+        <GridItem area={`main`} bg={`dodgerblue`}>
+          main
+        </GridItem>
+      </Grid>
     </>
-  )
-
-
-    
-    
-  
+  );
 }
 export default app;
