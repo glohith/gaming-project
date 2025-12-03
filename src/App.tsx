0bdd02bfff46ec898e6fbab3ce86ff2f1c36b0fd
glohith <varshini.1si16is061@gmail.com>
2025-12-03 06:24:45 +0530

    button size

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,13 @@ function App() {
 
   return (
 
-     <HStack wrap="wrap" gap="6">
-      <Button variant="solid">Solid</Button>
-      <Button variant="subtle">Subtle</Button>
-      <Button variant="surface">Surface</Button>
-      <Button variant="outline">Outline</Button>
-      <Button variant="ghost">Ghost</Button>
-      <Button variant="plain">Plain</Button>
+    <HStack wrap="wrap" gap="6">
+      <Button size="xs">Button (xs)</Button>
+      <Button size="sm">Button (sm)</Button>
+      <Button size="md">Button (md)</Button>
+      <Button size="lg">Button (lg)</Button>
+      <Button size="xl">Button (xl)</Button>
     </HStack>
-    
   )
 }
 
