62086de9ec6712f2b3f8d41199400b461497e567
glohith <glohith5214@gamil.com>
2025-12-11 06:48:49 +0530

    create a gneric data fetching hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -6,7 +6,7 @@ import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
-  const {games,error,isLoading} = useGames()
+  const {data,error,isLoading} = useGames()
   const Skeletons = [1,2,3,4,5,6,7,8]
   return (
     <>
@@ -14,7 +14,7 @@ const GameGrid = () => {
       <SimpleGrid columns={{sm:1, md:2, lg:3, xl:4}} padding={10} gap="25px">
         {isLoading && Skeletons.map(skeleton => <GameCardContainer><GameCardSkeleton key={skeleton} />
         </GameCardContainer>)}
-        {games?.map((game) => (
+        {data?.map((game) => (
           <GameCardContainer>
           <GameCard key={game.id} game={game}/>
           </GameCardContainer>
