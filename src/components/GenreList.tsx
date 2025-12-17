5a39c8d0a88854545806844c2049084a9ae9139a
glohith <glohith5214@gamil.com>
2025-12-18 04:55:10 +0530

    cleaning game cards and genre list

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,28 +1,40 @@
-
-
-import {  HStack, Image,  Link,  List, ListItem, Spinner,  } from '@chakra-ui/react'
-import useGenres, { type Genre } from '../hooks/useGenre'
-interface props{
-  onSelectGenre:(genre:Genre)=>(void)
-  selectedGenre: Genre|null
+import { Heading, HStack, Image, Link, List, ListItem, Spinner } from "@chakra-ui/react";
+import useGenres, { type Genre } from "../hooks/useGenre";
+interface props {
+  onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-
-const GenreList = ({selectedGenre,onSelectGenre}:props) => {
-    const {data,isLoading,error} = useGenres()
-    if(error) return null
-    if(isLoading) return <Spinner />
+const GenreList = ({ selectedGenre, onSelectGenre }: props) => {
+  const { data, isLoading, error } = useGenres();
+  if (error) return null;
+  if (isLoading) return <Spinner />;
   return (
+<>
+    <Heading fontSize="2xl" marginBottom={3}>Genres</Heading>
     <List.Root>
-        {data?.map(genre =><ListItem key={genre.id} paddingY={`5px`}>
-            <HStack>
-                <Image boxSize={`32px`} borderRadius={8} src={genre.image_background} />
-                <Link fontWeight={genre.id===selectedGenre?.id?`bold`:`normal`} fontSize={`lg`} onClick={() =>onSelectGenre(genre)}>{genre.name}</Link>
-            </HStack>
-        </ListItem>)}
-        </List.Root>
-    
-  )
-}
+      {data?.map((genre) => (
+        <ListItem key={genre.id} paddingY={`5px`}>
+          <HStack>
+            <Image
+              boxSize={`32px`}
+              borderRadius={8}
+              src={genre.image_background}
+              objectFit="cover"
+            />
+            <Link
+              fontWeight={genre.id === selectedGenre?.id ? `bold` : `normal`}
+              fontSize={`lg`}
+              onClick={() => onSelectGenre(genre)}
+            >
+              {genre.name}
+            </Link>
+          </HStack>
+        </ListItem>
+      ))}
+    </List.Root>
+    </>
+  );
+};
 
-export default GenreList
+export default GenreList;
